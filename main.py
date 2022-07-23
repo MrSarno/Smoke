a242f8668659bcb59017d2334df80bae57569012
MrSarno <email@sarno.dev>
2022-07-23 19:55:57 +0100

    Fixes #2

--- main.py
+++ main.py
@@ -8,7 +8,7 @@
 # This tool is used to verify whether a specified Steam user owns a particular game on Steam.                          #
 #                                                                                                                      #
 # It WILL NOT WORK until the included .env file has been amended to contain information required by Valve's API. The   #
-# required placeholders included in secrets.env. Check the GitHub repo if you have any questions about using Smoke.    #
+# required values have placeholders in secrets.env. Check the GitHub repo if you have any questions about using Smoke. #
 #                                                                                                                      #
 # https://github.com/MrSarno/Smoke                                                                                     #
 #                                                                                                                      #
--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@
 # This tool is used to verify whether a specified Steam user owns a particular game on Steam.                          #
 #                                                                                                                      #
 # It WILL NOT WORK until the included .env file has been amended to contain information required by Valve's API. The   #
-# required placeholders included in secrets.env. Check the GitHub repo if you have any questions about using Smoke.    #
+# required values have placeholders in secrets.env. Check the GitHub repo if you have any questions about using Smoke. #
 #                                                                                                                      #
 # https://github.com/MrSarno/Smoke                                                                                     #
 #                                                                                                                      #
@@ -19,6 +19,7 @@ import os
 
 # clear screen regardless of OS
 
+
 def clear_screen():
-    os.system('cls' if os.name == 'nt' else 'clear')
+    os.system("cls" if os.name == "nt" else "clear")
     print("\n")
